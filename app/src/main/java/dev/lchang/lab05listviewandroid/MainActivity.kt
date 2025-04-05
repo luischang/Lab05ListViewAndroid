2f362fc150de4397a5d386e2ed7a70ebea0d7558
DRM-Icpna234 <dirtyyr2012@gmail.com>
2025-04-05 10:56:48 -0500

    Adding ListView with OS names on activity_main.xml and creating it on MainActivity.kt.

--- app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
+++ app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
@@ -1,12 +1,20 @@
 package dev.lchang.lab05listviewandroid
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var arrSo = arrayOf(
+        "Android", "iPhone", "Windows Phone", "Blackberry", "Firefox OS", "Amazon World", "Nokia"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +25,22 @@ class MainActivity : AppCompatActivity() {
             insets
 
             //Hola Meza-Rosales
+        }
+
+        val lsvSistemasOperativos: ListView = findViewById<ListView>(R.id.lsvSO) as ListView
+
+        val adaptador: Any? =
+            ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1, arrSo)
+
+        lsvSistemasOperativos.adapter = adaptador as ListAdapter?
+
+        lsvSistemasOperativos.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(
+                applicationContext,
+                "Posición $position",
+                Toast.LENGTH_SHORT
+            ).show()
+
         }
     }
 }
