2de4e5b35632f37548f9a636ee6adf23bbe8c841
laboratorioesan <laboratorioesan@esan.edu.pe>
2025-04-05 11:36:39 -0500

    Terminado despues de un arduo trabajo con el error

--- app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
+++ app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
@@ -1,25 +1,39 @@
 package dev.lchang.lab05listviewandroid
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     var arrSO = arrayOf(
-        "Android", "iPhone","Windows Phone","Blackberry","FireFox OS","Amazon World","Nokia"
+        "Android", "iPhone", "Windows Phone", "Blackberry", "FireFox OS", "Amazon World", "Nokia"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Asegúrate de que este es el layout correcto que estás utilizando
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val lsvSistemasOperativos: ListView = findViewById(R.id.lsvSO)
+
+        // Crea el adaptador correctamente
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrSO)
+
+        // Asigna el adaptador a la ListView
+        lsvSistemasOperativos.adapter = adaptador
+
+        // Establece el listener para la acción de clic en el item
+        lsvSistemasOperativos.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(
+                applicationContext,
+                "Has seleccionado: ${arrSO[position]} en la posición: $position",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
