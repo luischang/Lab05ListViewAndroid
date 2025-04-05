804b62829c93ed858c73640dd3fd0cb40e594c70
MiguelApolaya <23100173@ue.edu.pe>
2025-04-05 11:08:49 -0500

    Creacion de rama

--- app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
+++ app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
@@ -1,6 +1,10 @@
 package dev.lchang.lab05listviewandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val lsvEquiposdefut: ListView = findViewById<View>(R.id.IsvSO) as ListView
+        val adpEquiposdefut = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrSO)
+        lsvEquiposdefut.adapter = adpEquiposdefut
+        lsvEquiposdefut.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(
+                applicationContext,
+                "La seleccion seleccionada es: " + arrSO[position],
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
