185af08b4aff071d90ba9d77c14511ebde59b808
AngelAguilar21 <angel789456123jesus@gmail.com>
2025-04-05 10:50:25 -0500

    paises

--- app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
+++ app/src/main/java/dev/lchang/lab05listviewandroid/MainActivity.kt
@@ -1,21 +1,46 @@
 package dev.lchang.lab05listviewandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 class MainActivity : AppCompatActivity() {
+    var arrPaises = arrayOf(
+        "Peru", "Bolivia", "Venezuela", "Colombia", "Argentina", "Chile"
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val listaPaises : ListView = findViewById<View>(R.id.lsvPaises) as ListView
+
+        val adaptador: Any? = ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1,arrPaises)
+
+        listaPaises.adapter = adaptador as ListAdapter?
+
+        listaPaises.setOnItemClickListener{ parent, view, position, id ->
+            Toast.makeText(
+                applicationContext,
+                "Posicion: $position",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+
+
+
+
+
     }
     //New Comment: Infantes - Aguilar
 
